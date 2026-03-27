90b453615d875d947bea8b1a9884cfbddf910b8b
sandumi <sandumidilthara729@example.com>
2026-03-27 23:19:56 +0530

    f

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,24 +2,21 @@ module.exports = {
   apps: [
     {
       name: "config-server",
-      script : "java -jar ./config-server/target/Config-Server-1.0.0.jar",
+      script: "java",
+      args: "-jar ./config-server/target/Config-Server-1.0.0.jar",
       log_file: "./logs/config-server.log",
-
     },
     {
       name: "service-registry",
-      script : "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
+      script: "java",
+      args: "-jar ./service-registry/target/Service-Registry-1.0.0.jar",
       log_file: "./logs/service-registry.log",
-
     },
     {
       name: "api-gateway",
-      script : "java -jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
+      script: "java",
+      args: "-jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
       log_file: "./logs/api-gateway.log",
-
     }
   ]
 }
-
-
-
